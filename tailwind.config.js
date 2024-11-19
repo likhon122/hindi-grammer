43dd664d022b42922b094a7fb6c1ebe8029eebd5
Likhon Islam <md.likhonislam2x@gmail.com>
2024-11-19 11:31:57 +0600

    Full webpage

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include all files in src directory
+    "./*.html" // Include all root-level HTML files
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
